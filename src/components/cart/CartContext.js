a45ea9acc5dc3a04a1f17ab5df7d37479656ef5e
¨lucia_ab¨ <luabramzon@gmail.com>
2022-02-25 16:15:15 -0300

    codigo

--- src/components/cart/CartContext.js
+++ src/components/cart/CartContext.js
@@ -1,13 +1,24 @@
 import { createContext, useState } from "react"
-import { Link } from "react-router-dom"
 import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore";
 export const CartContext=createContext()
 
-
-
 export const CartProvider = ({children})=>{
     const[items,setItems]=useState([])
+        
+    const[buyer,setBuyer]=useState({
+        name:"",
+        phone:"",
+        email:"",
+    })
+
+    const inputs=[
+        {label:"nombre",name:"name"},
+        {label:"phone",name:"phone"},
+        {label:"email",name:"email"}
+    ]
     
+    const [orderId,setOrderId]=useState(null)
+
     const deleteItem = (id) => {
         setItems(items.filter(item=>item.id!==id));
        
@@ -22,45 +33,26 @@ export const CartProvider = ({children})=>{
     }
 
     const totalCart=()=>{
-        
         return items.reduce((acum, prod)=> acum +( prod.quantity),0)
     
     }
 
-const addItem=(currentItem)=>{
-    const indice=items.findIndex(item=>item.id===currentItem.id)
+    const addItem=(currentItem)=>{
+        const indice=items.findIndex(item=>item.id===currentItem.id)
 
-    if(indice >-1){ 
-        alert("producto duplicado")
-        const oldQuantity= items[indice].quantity
-        items[indice].quantity= oldQuantity+currentItem.quantity
-       /*  setItems([...items,currentItem]) */
-    } else{ 
-        setItems([...items,currentItem])
-    }
-        
+            if(indice >-1){ 
+                alert("producto duplicado")
+                const oldQuantity= items[indice].quantity
+                items[indice].quantity= oldQuantity+currentItem.quantity
+            } else{ 
+                setItems([...items,currentItem])
+            }
     };
-
-    
-    const[buyer,setBuyer]=useState({
-        name:"",
-        phone:"",
-        email:"",
-    })
-
-    const inputs=[
-        {label:"nombre",name:"name"},
-        {label:"phone",name:"phone"},
-        {label:"email",name:"email"}
-    ]
 
     const onChange=(event)=>{
         setBuyer({...buyer,[event.target.name]:event.target.value})
     }
-    
-
-    const [orderId,setOrderId]=useState(null)
-
+ 
     const sendOrder = () =>{
         const order={
             buyer,
@@ -76,7 +68,6 @@ const addItem=(currentItem)=>{
             const docRef=doc(db,"items",item.id);
             updateDoc(docRef,{stock:item.stock-item.quantity})
             })
-
     }
     return (
         <CartContext.Provider value={{
@@ -91,7 +82,6 @@ const addItem=(currentItem)=>{
             onChange,
             orderId,
             sendOrder,
-            
         }
          }>
             {children}
